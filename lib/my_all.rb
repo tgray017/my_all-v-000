ec0d1aa37c084876a0598222c0b1b328e9de76b5
tgray017 <tgray017@gmail.com>
2018-09-22 19:35:47 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,7 +7,7 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
-  block_return_values.include?(false)!
+  block_return_values.include?(false)!!
 end
 
 my_all?([1,2,3]) {|i| i < 2}
